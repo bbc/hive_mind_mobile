59aa4d139d8183134cf97f750c51871e03d6bdb1
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-11-30 15:54:53 +0000

    Updated test to check that the device uses the model as its name

--- spec/models/hive_mind_mobile/plugin_spec.rb
+++ spec/models/hive_mind_mobile/plugin_spec.rb
@@ -4,11 +4,11 @@ module HiveMindMobile
   RSpec.describe Plugin, type: :model do
     describe '#serial' do
       let(:hive_plugins) {
-        Plugin.create( serial: '123456789' )
+        Plugin.create( serial: '123456789', model: 'Test Phone' )
       }
 
       it 'returns the hostname' do
-        expect(hive_plugins.name).to eq '123456789'
+        expect(hive_plugins.name).to eq 'Test Phone'
       end
     end
   end
