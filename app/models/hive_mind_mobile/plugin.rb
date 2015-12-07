bd7ceb272150703d5ca7b387239bead710833a3e
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-12-07 10:35:04 +0000

    Returns reference to device in the create method

--- app/models/hive_mind_mobile/plugin.rb
+++ app/models/hive_mind_mobile/plugin.rb
@@ -18,16 +18,17 @@ module HiveMindMobile
       device = super(*args)
 
       if args.first.key? 'hive_id'
-        if device.device_hive.length
+        if device.device_hive.length > 0
           device.device_hive.last.end_timestamp = Time.now
         end
         device.device_hive << DeviceHive.create(hive_id: args.first['hive_id'], start_timestamp: DateTime.now())
       end
-
+      device
     end
 
     def hive_id
       self.device_hive.where(end_timestamp: nil).order(start_timestamp: :desc)
     end
+
   end
 end
